b5612d9027e0350e72a55f338c54cb241369899a
berto3333 <damswings@gmail.com>
2026-06-28 09:17:48 +0200

    actualiz

--- android/app/src/main/java/com/oposvozcap/app/MainActivity.java
+++ android/app/src/main/java/com/oposvozcap/app/MainActivity.java
@@ -1,5 +1,15 @@
-package com.oposvozcap.app;
+import com.getcapacitor.community.speechrecognition.SpeechRecognition;
 
-import com.getcapacitor.BridgeActivity;
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-public class MainActivity extends BridgeActivity {}
+        this.init(
+            savedInstanceState,
+            new ArrayList<Class<? extends Plugin>>() {{
+                add(SpeechRecognition.class);
+            }}
+        );
+    }
+}
